9378a0b5c80812dba5411006dcb76c16f7d23d20
Graham Esau <gesau@hotmail.co.uk>
2019-08-14 08:30:03 +0100

    route_with_openapi wip

--- rocket-okapi-codegen/src/lib.rs
+++ rocket-okapi-codegen/src/lib.rs
@@ -6,6 +6,7 @@ extern crate syn;
 extern crate proc_macro;
 
 mod openapi_attr;
+mod routes_with_openapi;
 
 use proc_macro::TokenStream;
 use syn::Ident;
@@ -16,6 +17,11 @@ pub fn openapi(args: TokenStream, mut input: TokenStream) -> TokenStream {
     // we just need to append to it.
     input.extend(openapi_attr::parse(args, input.clone()));
     input
+}
+
+#[proc_macro]
+pub fn routes_with_openapi(input: TokenStream) -> TokenStream {
+    routes_with_openapi::parse(input)
 }
 
 fn get_add_operation_fn_name(route_fn_name: &Ident) -> Ident {
