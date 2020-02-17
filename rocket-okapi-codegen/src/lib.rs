a6ef9487b99d4c10ed3e48c6cec89b582f361c8e
Graham Esau <gesau@hotmail.co.uk>
2020-02-17 18:59:00 +0000

    Preserve span info when using #[openapi]
    (issue #12)

--- rocket-okapi-codegen/src/lib.rs
+++ rocket-okapi-codegen/src/lib.rs
@@ -13,6 +13,8 @@ use syn::Ident;
 
 #[proc_macro_attribute]
 pub fn openapi(args: TokenStream, mut input: TokenStream) -> TokenStream {
+    input = preserve_span_information(input);
+
     // We don't need to modify/replace the input TokenStream,
     // we just need to append to it.
     input.extend(openapi_attr::parse(args, input.clone()));
@@ -22,6 +24,16 @@ pub fn openapi(args: TokenStream, mut input: TokenStream) -> TokenStream {
 #[proc_macro]
 pub fn routes_with_openapi(input: TokenStream) -> TokenStream {
     routes_with_openapi::parse(input)
+}
+
+fn preserve_span_information(input: TokenStream) -> TokenStream {
+    // Outputting the input unmodified would cause its span information to be
+    // lost when being consumed by other macros. But parsing it in then quoting
+    // it back out causes span information to be preserved.
+    // See https://github.com/GREsau/okapi/issues/12
+    // and https://github.com/rust-lang/rust/issues/43081
+    let parsed_input: syn::Item  = syn::parse(input).unwrap();
+    quote!(#parsed_input).into()
 }
 
 fn get_add_operation_fn_name(route_fn_name: &Ident) -> Ident {
