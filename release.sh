7208acb96060c38a09cf739c72046038f038ba93
Kristoffer Ödmark <kristoffer.odmark90@gmail.com>
2021-07-07 16:53:14 +0200

    --amend

--- release.sh
+++ release.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cargo release --manifest-path okapi/Cargo.toml patch --dry-run
+sleep 60
 cargo release --manifest-path rocket-okapi-codegen/Cargo.toml patch --dry-run
+sleep 60
 cargo release --manifest-path rocket-okapi/Cargo.toml patch --dry-run
